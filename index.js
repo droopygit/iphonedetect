702a8d391576a5c78e3cef2237f26f6199815055
Yann Faure <yann.faure@slowsense.com>
2023-03-21 19:10:35 +0100

    add models

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function init() {
+import models from './models.json' assert { type: "json" };
+
+export function init() {
     const screenWidth = window.screen.width;
     const screenHeight = window.screen.height;
-    document.getElementById("device-model").innerHTML = `${screenWidth} x ${screenHeight}`;
+    const resolution = `${screenWidth}x${screenHeight}`;
+    const deviceModels = models.filter((device) => device.resolution === resolution);
+    document.getElementById("resolution").innerHTML = resolution;
+    document.getElementById("models").innerHTML = deviceModels.map((device) => device.model).join(", ");
+
 }
